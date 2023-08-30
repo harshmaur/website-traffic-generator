344501ab841bb6d72db181a6d3940fe1b73a919c
harshmaur <harshmaur@gmail.com>
2023-08-30 11:23:57 +0530

    fix(): print input

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@
  */
 
 // For more information, see https://docs.apify.com/sdk/js
-import { Actor } from 'apify';
+import { Actor, log } from 'apify';
 // For more information, see https://crawlee.dev
 import { PlaywrightCrawler } from 'crawlee';
 import { router } from './routes.js';
@@ -17,6 +17,8 @@ await Actor.init();
 const input = await Actor.getInput<{
     startUrls: string[]
 }>();
+
+log.info(JSON.stringify(input));
 
 if (input === null) {
     await Actor.fail('Please provide input options and then run the actor');
