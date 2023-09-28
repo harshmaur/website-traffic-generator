b7d5d5b02c107a4db5d88c0b75d642f7f9055a1e
harshmaur <harshmaur@gmail.com>
2023-09-28 15:21:26 +0530

    fix(): actor

--- src/main.ts
+++ src/main.ts
@@ -8,32 +8,81 @@
 // For more information, see https://docs.apify.com/sdk/js
 import { Actor, log } from 'apify';
 // For more information, see https://crawlee.dev
-import { PlaywrightCrawler } from 'crawlee';
+import { Configuration, PlaywrightCrawler, PlaywrightCrawlingContext } from 'crawlee';
 import { router } from './routes.js';
 
-// Initialize the Apify SDK
-await Actor.init();
+import 'dotenv/config';
 
-const input = await Actor.getInput<{
-    startUrls: string[]
-}>();
-
-log.info(JSON.stringify(input));
-
-if (input === null) {
-    await Actor.fail('Please provide input options and then run the actor');
+type InputType = {
+    startUrls: string[];
+    maxPageWaitSeconds: number;
+    minPageWaitSeconds: number;
+    enqueueLinks: boolean;
+    multiply: number;
+    parallelize: boolean;
 }
+const config = Configuration.getGlobalConfig();
+config.set('persistStorage', false);
 
-const { startUrls } = input!;
+export type CrawlingContext = PlaywrightCrawlingContext<InputType>;
 
-const proxyConfiguration = await Actor.createProxyConfiguration();
+await Actor.main(async () => {
+    const input = await Actor.getInput<InputType>();
 
-const crawler = new PlaywrightCrawler({
-    proxyConfiguration,
-    requestHandler: router,
+    if (!input) {
+        await Actor.fail('Please provide input options and then run the actor');
+        return;
+    }
+
+    const {
+        startUrls,
+        maxPageWaitSeconds = 5,
+        minPageWaitSeconds = 1,
+        multiply = 1,
+        enqueueLinks = false,
+        parallelize = true,
+    } = input;
+
+    const proxyConfiguration = await Actor.createProxyConfiguration();
+
+    const crawler = new PlaywrightCrawler({
+        log,
+        requestQueue: await Actor.openRequestQueue('website-traffic'),
+        proxyConfiguration,
+        requestHandler: router,
+        maxConcurrency: parallelize ? undefined : 1,
+    });
+
+    const uniqueMultiplies = Array.from({ length: multiply }, (_, i) => i + 1);
+
+    for (let i = 0; i < uniqueMultiplies.length; i++) {
+        log.setOptions({
+            prefix: `Loop: ${i + 1}`,
+        });
+        await crawler.addRequests(
+            startUrls.map((url) => {
+                try {
+                    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+                    const a = new URL(url);
+                } catch (error) {
+                    log.error(`Invalid URL: ${url}`);
+                    throw error;
+                }
+
+                return ({
+                    url,
+                    uniqueKey: `${i + 1}${url}`,
+                    userData: {
+                        minPageWaitSeconds,
+                        maxPageWaitSeconds,
+                        enqueueLinks,
+                        multiply,
+                        startUrls,
+                    },
+                });
+            }),
+        );
+
+        await crawler.run();
+    }
 });
-
-await crawler.run(startUrls);
-
-// Exit successfully
-await Actor.exit();
--- src/routes.ts
+++ src/routes.ts
@@ -1,26 +1,35 @@
-import { Dataset, createPlaywrightRouter } from 'crawlee';
+import { createPlaywrightRouter } from 'crawlee';
+import { CrawlingContext } from './main';
 
 export const router = createPlaywrightRouter();
 
-router.addDefaultHandler(async ({ page, enqueueLinks, log }) => {
-    log.info(`enqueueing new URLs`);
-    const url = page.url();
-    log.info(`URL: ${url}`);
-    if (!url) return;
-    const { origin } = new URL(url);
-    log.info(`origin: ${origin}`);
-    await enqueueLinks({
-        globs: [`${origin}/*`],
-        label: 'detail',
-    });
-});
+router.addDefaultHandler(async ({ page, enqueueLinks, request, log }: CrawlingContext) => {
+    const { enqueueLinks: shouldEnqueue, maxPageWaitSeconds, minPageWaitSeconds, multiply, startUrls } = request.userData;
 
-router.addHandler('detail', async ({ request, page, log }) => {
-    const title = await page.title();
-    log.info(`${title}`, { url: request.loadedUrl });
+    log.info(`Crawling URL: ${request.url}`);
+    await page.waitForLoadState('networkidle');
+    const timeoutRandom = Math.random() * (maxPageWaitSeconds - minPageWaitSeconds) + minPageWaitSeconds;
+    log.info(`${request.url} - Waiting for ${timeoutRandom} seconds`);
 
-    await Dataset.pushData({
-        url: request.loadedUrl,
-        title,
-    });
+    if (shouldEnqueue) {
+        log.info(`${request.url} - Extracting and adding them to crawler queue`);
+        await enqueueLinks({
+            selector: 'a',
+            transformRequestFunction: (_request) => {
+                _request.uniqueKey = multiply + _request.url;
+                _request.userData = {
+                    minPageWaitSeconds,
+                    maxPageWaitSeconds,
+                    multiply,
+                    enqueueLinks: shouldEnqueue,
+                    startUrls,
+                };
+                return _request;
+            },
+            strategy: 'same-domain',
+            baseUrl: new URL(request.url).origin,
+        });
+    }
+
+    await page.waitForTimeout(timeoutRandom * 1000);
 });
